6d9e6f417c2b5cf71855322159a7a82f12adb103
Shamim65 <kmshamim65@gmail.com>
2023-08-24 23:51:42 +0600

    Range sum query

--- Module 3 Prefix Sum and Binary Search/Range_sum_query.cpp
+++ Module 3 Prefix Sum and Binary Search/Range_sum_query.cpp
@@ -2,24 +2,29 @@
 using namespace std;
 int main()
 {
-    int n,q;
+    long long int n,q;
     cin>>n>>q;
-    int a[n];
+    long long int a[n];
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
+    long long int pre[n];
+    pre[0]=a[0];
+    for(int i=1;i<n;i++)
+    {
+        pre[i]=a[i]+pre[i-1];
+    }
+
     while(q--)
     {
         int l,r;
         cin>>l>>r;
         l--;
         r--;
-        int sum=0;
-        for(int i=l;i<=r;i++)
-        {
-            sum+=a[i];
-        }
+        long long int sum;
+        if(l==0) sum=pre[r];
+        else sum=pre[r]-pre[l-1];
         cout<<sum<<endl;
     }
     return 0;

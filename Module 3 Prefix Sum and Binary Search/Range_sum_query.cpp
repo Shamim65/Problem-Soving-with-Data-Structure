ce1ad97778a79bf6d6f093b3215f83a5a74493fd
Shamim65 <kmshamim65@gmail.com>
2023-08-28 23:00:14 +0600

    Change

--- Module 3 Prefix Sum and Binary Search/Range_sum_query.cpp
+++ Module 3 Prefix Sum and Binary Search/Range_sum_query.cpp
@@ -9,23 +9,5 @@ int main()
     {
         cin>>a[i];
     }
-    long long int pre[n];
-    pre[0]=a[0];
-    for(int i=1;i<n;i++)
-    {
-        pre[i]=a[i]+pre[i-1];
-    }
-
-    while(q--)
-    {
-        int l,r;
-        cin>>l>>r;
-        l--;
-        r--;
-        long long int sum;
-        if(l==0) sum=pre[r];
-        else sum=pre[r]-pre[l-1];
-        cout<<sum<<endl;
-    }
     return 0;
 }

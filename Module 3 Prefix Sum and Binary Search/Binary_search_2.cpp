6bd86a7aa0daf67c0b83731cba2d341e2da827db
Shamim65 <kmshamim65@gmail.com>
2023-08-29 23:49:30 +0600

    change

--- Module 3 Prefix Sum and Binary Search/Binary_search_2.cpp
+++ Module 3 Prefix Sum and Binary Search/Binary_search_2.cpp
@@ -14,27 +14,5 @@ int main()
     int l=0;
     int r=n-1;
     bool flag=false;
-    while(l<=r)
-    {
-        int mid_index=(l+r)/2;
-        if(a[mid_index]==x)
-        {
-            flag=true;
-            break;
-        }
-        if(x>a[mid_index])
-        {
-            // dane jao
-            l=mid_index+1;
-        }
-        else 
-        {
-            // bame jao
-            r=mid_index-1;
-        }
-    }
-    cout<<l<<" "<<r<<endl;
-    if(flag==true) cout<<"YES"<<endl;
-    else cout<<"NO"<<endl;
-    return 0;
+    
 }

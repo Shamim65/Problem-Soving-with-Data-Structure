8c77e618493bef0d7fab9b2c054ca12389077fd0
Shamim65 <kmshamim65@gmail.com>
2023-08-31 22:22:22 +0600

    change

--- Module 5 Singly Linked List/create_a_node.cpp
+++ Module 5 Singly Linked List/create_a_node.cpp
@@ -8,17 +8,6 @@ class Node
 };
 int main()
 {
-    Node a,b;
-
-    a.val=10;
-    b.val=20;
-
-    a.next=&b;
-    b.next=NULL;
-
-    cout<<a.val<<endl;
-    cout<<b.val<<endl;
-    cout<<(*a.next).val<<endl;
-    cout<<a.next->val<<endl;
+    
     return 0;
 }

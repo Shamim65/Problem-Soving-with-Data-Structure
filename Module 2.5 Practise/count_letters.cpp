5db6ff693b14d90a8436c94dc6e3e070051d5c01
Shamim65 <kmshamim65@gmail.com>
2023-09-01 23:32:10 +0600

    change

--- Module 2.5 Practise/count_letters.cpp
+++ Module 2.5 Practise/count_letters.cpp
@@ -5,16 +5,6 @@ int main()
     string S;
     cin >> S;
 
-    vector<int> frequency(26, 0); // Initialize a vector of size 26 for lowercase English letters
-
-    for (char c : S) {
-        frequency[c - 'a']++; // Increment the frequency of the corresponding letter
-    }
-
-    for (int i = 0; i < 26; i++) {
-        if (frequency[i] > 0) {
-            cout << char('a' + i) << " : " << frequency[i] << endl;
-        }
-    }
+    
     return 0;
 }

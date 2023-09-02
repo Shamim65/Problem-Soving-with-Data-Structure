0ae16af07344ba82dc25986e02c35f68dbeab77c
Shamim65 <kmshamim65@gmail.com>
2023-09-02 23:30:50 +0600

    Prefix sum

--- Module 4 Assignment 1/prefix_sum.cpp
+++ Module 4 Assignment 1/prefix_sum.cpp
@@ -10,20 +10,38 @@ int main()
     {
         cin>>v[i];
     }
-    // pre[0]=v[0];
-    // for(int i=1;i<n;i++)
+    pre[0]=v[0];
+    for(int i=1;i<n;i++)
+    {
+        pre[i]=v[i]+pre[i-1];
+    }
+    while(q--)
+    {
+        int l,r;
+        cin>>l>>r;
+        l--;//1 base index ke 0 base index e convert
+        r--;//1 base index ke 0 base index
+        if(l==0)
+        {
+            cout<<pre[r]<<endl;
+        }
+        else
+        {
+            cout<<pre[r]-pre[l-1]<<endl;
+        }
+        
+
+
+    }
+    // int sum=0;
+    // for(int i=0;i<n;i++)
     // {
-    //     pre[i]=v[i]+pre[i-1];
+    //     sum+=v[i];
+    //     pre[i]=sum;
     // }
-    int sum=0;
-    for(int i=0;i<n;i++)
-    {
-        sum+=v[i];
-        pre[i]=sum;
-    }
-    for(int i=0;i<n;i++)
-    {
-        cout<<pre[i]<<" ";
-    }
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<pre[i]<<" ";
+    // }
     return 0;
 }

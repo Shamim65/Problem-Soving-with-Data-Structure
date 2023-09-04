e0bd4789a021d1b3dad7d1d46e57ee02aaa9c348
Shamim65 <kmshamim65@gmail.com>
2023-09-04 22:42:19 +0600

    Insert it

---
+++ Module 4 Assignment 1/insert_it.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n;
+    vector<int>A(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>A[i];
+    }
+    cin>>m;
+    vector<int>B(m);
+    for(int i=0;i<n;i++)
+    {
+        cin>>B[i];
+    }
+    return 0;
+}

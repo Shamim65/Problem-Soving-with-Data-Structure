6214cd443eff48ca17edeb01cbf54940bc0e7be9
Shamim65 <kmshamim65@gmail.com>
2023-09-05 00:06:09 +0600

    Duplicate

--- Module 4 Assignment 1/Duplicate.cpp
+++ Module 4 Assignment 1/Duplicate.cpp
@@ -2,6 +2,31 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    vector<int>frequency(1000001,0);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+        frequency[v[i]]++;
+    }
+    bool Duplicate=false;
+    for(int i=0;i<n;i++)
+    {
+        if(frequency[v[i]]>1)
+        {
+            Duplicate=true;
+            break;
+        }
+    }
+    if(Duplicate)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
     return 0;
 }

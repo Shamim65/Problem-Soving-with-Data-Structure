646ab11a4a5f8b1b14acbe3644fd451f4ed629cb
Shamim65 <kmshamim65@gmail.com>
2023-09-12 23:52:06 +0600

    Get prefix sum

--- Module 4 Assignment 1/Get_Prefix_Sum.cpp
+++ Module 4 Assignment 1/Get_Prefix_Sum.cpp
@@ -10,6 +10,14 @@ int main()
     {
         cin>>v[i];
     }
-    
+    pre[0]=v[0];
+    for(int i=1;i<n;i++)
+    {
+        pre[i]=v[i]+pre[i-1];
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        cout<<pre[i]<<" ";
+    }
     return 0;
 }
